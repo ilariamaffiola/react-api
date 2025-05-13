221e531a65474c1c991e507bdad385a1f3d9e4b6
Ilaria <maffiolailaria1@gmail.com>
2025-05-13 15:38:10 +0200

    [UPD] refactor fetchActress function to log response data and implement useEffect for data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 function App() {
 
   const [actress, setActress] = useState([]);
   const fetchActress = () => {
-    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => setActress(response.data));
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => {
+      console.log(response.data);
+    });
   };
+  useEffect(() => {
+    fetchActress();
+  }, [])
   
 
   return (
     <>
       <div>
-        
       </div>
     </>  
   )
