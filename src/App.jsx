9fde4fb3e9a4ee65869d50d4d968db0a1b2734c6
Ilaria <maffiolailaria1@gmail.com>
2025-05-13 15:31:51 +0200

    [UPD]axios dependency and implement fetchActress function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 
 function App() {
+
+  const [actress, setActress] = useState([]);
+  const fetchActress = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => setActress(response.data));
+  };
   
 
   return (
     <>
+      <div>
+        
+      </div>
     </>  
   )
 }
