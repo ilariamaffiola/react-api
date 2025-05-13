bc20c5ba8f1e26850f3d91d7303aaa94d3fec568
Ilaria <maffiolailaria1@gmail.com>
2025-05-13 16:01:50 +0200

    [UPD] update fetchActress function to set actress state and enhance UI layout for displaying actress data

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
 
   const [actress, setActress] = useState([]);
   const fetchActress = () => {
-    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => {
-      console.log(response.data);
-    });
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((response) => {setActress(response.data)});
   };
   useEffect(() => {
     fetchActress();
@@ -16,6 +14,28 @@ function App() {
   return (
     <>
       <div>
+        <div className="container-fluid">
+          <h1>CAST FATCHING</h1>
+
+          <div className="row g-4">
+                {actress.map((item) => (
+                  <div className="col-3">
+                  <div className="card" key={item.id}>
+                    <div className="card-img">
+                      <img src={item.image} className="card-img-top" alt="..." />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <p className='card-text'>{item.birth_year}</p>
+                      <p className='card-text'>{item.nationality}</p>
+                      <p className="card-text">{item.biography}</p>
+                    </div>
+                  </div>
+                  </div>
+                ))}
+              
+          </div>
+        </div>
       </div>
     </>  
   )
